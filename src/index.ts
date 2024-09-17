d1ecc6229b126bcbe5c46fa1e19093eef629ae16
odan <odan3240@gmail.com>
2024-09-17 11:44:39 +0900

    add PoC

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+PerformanceObserver;
+window.PerformanceObserver;
+
+// @ts-expect-error
+PerformanceObserver.supportedEntryTypes;
+
+// @ts-expect-error
 window.PerformanceObserver.supportedEntryTypes;
